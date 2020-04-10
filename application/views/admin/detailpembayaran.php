241394c34ff3c570effdcaa414844814d951c7d4
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2020-04-10 21:47:23 +0700

    Update detailpembayaran.php

--- application/views/admin/detailpembayaran.php
+++ application/views/admin/detailpembayaran.php
@@ -5,81 +5,103 @@
 <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
 -->
 <!-- DataTales Example -->
-<div class="card shadow mb-4">
+<div class="card shadow mb-4"> 
   <div class="card-header py-3">
-    <h6 class="m-0 font-weight-bold text-primary">Tabel Detail Pemasukan</h6>
+    <h6 class="m-0 font-weight-bold text-primary">Detail Pembayaran</h6>
   </div>
   <div class="card-body">
     <div class="table-responsive">
-      <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-        <thead>
-          <!-- <tr>
-            <th>NO</th> -->
-            <th>No Pembayaran</th>
-            <th>NIK</th>
-            <th>Nama</th>
-            <th>Pembayaran Bulan</th>
-            <!-- <th>Nominal</th> -->
-            <th>Tanggal Pembayaran Bulan januari</th>
-            <th>Tanggal Pembayaran Bulan Februari</th>
-            <th>Tanggal Pembayaran Bulan Maret</th>
-            <th>Tanggal Pembayaran Bulan April</th>
-            <th>Tanggal Pembayaran Bulan Mei</th>
-            <th>Tanggal Pembayaran Bulan juni</th>
-            <th>Tanggal Pembayaran Bulan juli</th>
-            <th>Tanggal Pembayaran Bulan Agustus</th>
-            <th>Tanggal Pembayaran Bulan September</th>
-            <th>Tanggal Pembayaran Bulan Oktober</th>
-            <th>Tanggal Pembayaran Bulan November</th>
-            <th>TanggalPembayaran  Bulan Desember</th>
-            <th>Tagihan</th> 
-          </tr>
-        </thead>
-
-        <tbody>
+      <table border = "0" id="dataTable" cellspacing="0">
+     <form>
         <?php 
           $no = 1;
           $sum=0;
           foreach($detailpembayaran as $b){ 
         ?>
-          <tr>
+         
             <!-- <td><?php echo $no++; ?></td> -->
-            <td><?php echo $b->no_pembayaran; ?></td>   
-            <td><?php echo $b->nik; ?></td>
-            <td><?php echo $b->nama_warga; ?></td>
-            <!-- <td><?php echo $b->tanggal; ?></td> -->
-            <td><?php echo $b->jenis_warga; ?></td>
-            <td><?= $b->bulan_januari ?>(<?= $b->tanggal_bulan_januari ?>)</td>
-            <td><?= $b->bulan_februari ?>(<?= $b->tanggal_bulan_februari ?>)</td>
-            <td><?= $b->bulan_maret ?>(<?= $b->tanggal_bulan_maret ?>)</td>
-            <td><?= $b->bulan_april ?>(<?= $b->tanggal_bulan_april ?>)</td>
-            <td><?= $b->bulan_mei ?>(<?= $b->tanggal_bulan_mei ?>)</td>
-            <td><?= $b->bulan_juni ?>(<?= $b->tanggal_bulan_juni ?>)</td>
-            <td><?= $b->bulan_juli ?>(<?= $b->tanggal_bulan_juli ?>)</td>
-            <td><?= $b->bulan_agustus ?>(<?= $b->tanggal_bulan_agustus ?>)</td>
-            <td><?= $b->bulan_september ?>(<?= $b->tanggal_bulan_september ?>)</td>
-            <td><?= $b->bulan_oktober ?>(<?= $b->tanggal_bulan_oktober ?>)</td>
-            <td><?= $b->bulan_november ?>(<?= $b->tanggal_bulan_november ?>)</td>
-            <td><?= $b->bulan_desember ?>(<?= $b->tanggal_bulan_desember ?>)</td>
-            <!-- <td><?php echo $b['gambar'] ?></td> -->
-            <!-- <td><img src="<?php echo base_url('/uploads/gambar/'.$b['gambar']);?>" height="50px" width="50px"></td> -->
-            <td>
-            <?php $sum=$sum+$b['nominal']?>
-              <!-- <?php echo anchor('admin/edit_iuran_keluar/'.$b['no_pengeluaran'],'Edit'); ?> | |  
-                 <?php echo anchor('admin/hapus_iuran_keluar/'.$b['no_pengeluaran'],'Hapus'); ?>    -->
-            </td>
+            <tr>
+              <td> Nomor Pembayaran </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo $b->no_pembayaran; ?></td>
+            </tr>
+            <tr>
+              <td> NIK </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo $b->nik ?></td>
+            </tr>
+            <tr>
+              <td> Nama Warga </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo $b->nama_warga; ?></td>
+            </tr>
+            <tr>
+              <td> Jenis Warga </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo $b->jenis_warga; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Januari </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_januari; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Februari </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_februari; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Maret </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_maret; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan April </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_april; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Mei </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_mei; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Juni </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_juni; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Juli </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_juli; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Agustus </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_agustus; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan September </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_september; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Oktober </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_oktober; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan November </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_november; ?></td>
+            </tr>
+            <tr>
+              <td> Tanggal Pembayaran Bulan Desember </td>
+              <td>&nbsp;&nbsp;:&nbsp;&nbsp;</td>
+              <td><?php echo  $b->tanggal_bulan_desember; ?></td>
           </tr>
         <?php } ?>
-        <tr>
-      <td colspan="2" rospan="4">Total</td>
-      <!-- <td><?php echo $sum;?> </td> -->
-      <td></td>
-      <td></td>
-      <td><?php echo $sum;?> </td>
-      <td></td>
-      <td></td>
-    </tr>
-        </tbody>
+          </form>
       </table>
 </div>
   </div>
