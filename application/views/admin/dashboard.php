2b684a545a4676c04324ede08a3342b642f6401d
ivanianonita <42725307+ivanianonita@users.noreply.github.com>
2020-09-02 18:04:53 +0700

    updatee

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -11,7 +11,7 @@
 <div class="row">
 
   <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <!-- <div class="col-xl-4 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
       <div class="card-body">
         <div class="row no-gutters align-items-center">
@@ -26,12 +26,12 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
 
 
   <!-- Earnings (Monthly) Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <!-- <div class="col-xl-4 col-md-6 mb-4">
     <div class="card border-left-info shadow h-100 py-2">
       <div class="card-body">
         <div class="row no-gutters align-items-center">
@@ -60,10 +60,10 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
   <!-- Pending Requests Card Example -->
-  <div class="col-xl-3 col-md-6 mb-4">
+  <!-- <div class="col-xl-4 col-md-6 mb-4">
     <div class="card border-left-warning shadow h-100 py-2">
       <div class="card-body">
         <div class="row no-gutters align-items-center">
@@ -83,98 +83,29 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 </div>
-
-<!-- Content Row -->
-
 <div class="row">
 
-  <!-- Area Chart -->
-  <div class="col-xl-4 col-lg-5">
-    <div class="card shadow mb-4">
-      <!-- Card Header - Dropdown -->
-      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-        <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-        <div class="dropdown no-arrow">
-          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-            <div class="dropdown-header">Dropdown Header:</div>
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </div>
-      </div>
-      <!-- Card Body -->
-      <div class="card-body">
-        <div class="chart-area">
-          <canvas id="myAreaChart"></canvas>
-        </div>
-      </div>
-    </div>
+  <div class="col-xl-12 col-lg-5">
+    <div id="chartContainer" style="height: 300px; width: 100%;"></div>
+  </div>
+
+
+  <div class="col-xl-12 col-lg-5">
+    <div id="chartPengeluaranPemasukan" style="height: 300px; width: 100%;"></div>
   </div>
 
   <!-- Pie Chart -->
-  <div class="col-xl-4 col-lg-5">
+  <div class="col-xl-6 col-lg-5">
+    <br><br><br>
     <div id="education"></div>
   </div>
 
   <!-- Pie Chart -->
-  <div class="col-xl-4 col-lg-5">
+  <div class="col-xl-6 col-lg-5">
+    <br><br><br>
     <div id="job"></div>
-  </div>
-</div>
-<!-- Content Row -->
-<div class="row">
-
-  <!-- Content Column -->
-  <div class="col-lg-6 mb-4">
-
-    <!-- Project Card Example -->
-    <div class="card shadow mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
-      </div>
-      <div class="card-body">
-
-      </div>
-    </div>
-
-    <!-- Color System -->
-
-  </div>
-
-  <div class="col-lg-6 mb-4">
-
-    <!-- Illustrations -->
-    <div class="card shadow mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-      </div>
-      <div class="card-body">
-        <div class="text-center">
-          <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;" src="img/undraw_posting_photo.svg" alt="">
-        </div>
-        <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a constantly updated collection of beautiful svg images that you can use completely free and without attribution!</p>
-        <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on unDraw &rarr;</a>
-      </div>
-    </div>
-
-    <!-- Approach -->
-    <div class="card shadow mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Development Approach</h6>
-      </div>
-      <div class="card-body">
-        <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce CSS bloat and poor page performance. Custom CSS classes are used to create custom components and custom utility classes.</p>
-        <p class="mb-0">Before working with this theme, you should become familiar with the Bootstrap framework, especially the utility classes.</p>
-      </div>
-    </div>
-
   </div>
 </div>
 
@@ -225,6 +156,103 @@ var chartKerjaan = new CanvasJS.Chart("job", {
 	}]
 });
 chartKerjaan.render();
+
+var chart = new CanvasJS.Chart("chartContainer", {
+	theme:"light2",
+	animationEnabled: true,
+	title:{
+		text: "Tren Pemasukkan & Pengeluaran RT - <?= $this->session->userdata('rt')?>"
+	},
+	axisY :{
+		includeZero: false,
+		title: "Jumlah (Rp.)"
+	},
+	toolTip: {
+		shared: "true"
+	},
+	legend:{
+		cursor:"pointer",
+		itemclick : toggleDataSeries
+	},
+  data: [{
+		type: "spline",
+    name: "Pemasukan",
+    markerSize: 5,
+    showInLegend: true,
+		visible: true,
+		yValueFormatString: "Rp#,##0.##",
+		dataPoints: <?php echo json_encode($jumlahPemasukan, JSON_NUMERIC_CHECK); ?>
+	},
+  {
+		type: "spline",
+    name: "Pengeluaran",
+    markerSize: 5,
+    showInLegend: true,
+		visible: true,
+		yValueFormatString: "Rp#,##0.##",
+		dataPoints: <?php echo json_encode($jumlahPengeluaran, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+
+
+var chart = new CanvasJS.Chart("chartPengeluaranPemasukan", {
+	animationEnabled: true,
+	title:{
+    text: "Laporan Pemasukkan & Pengeluaran RT - <?= $this->session->userdata('rt')?>"
+	},	
+	axisY: {
+		title: "Jumlah (Rp.)",
+		titleFontColor: "#4F81BC",
+		lineColor: "#4F81BC",
+		labelFontColor: "#4F81BC",
+		tickColor: "#4F81BC"
+	},
+	// axisY2: {
+	// 	title: "Millions of Barrels/day",
+	// 	titleFontColor: "#C0504E",
+	// 	lineColor: "#C0504E",
+	// 	labelFontColor: "#C0504E",
+	// 	tickColor: "#C0504E"
+	// },	
+	toolTip: {
+		shared: true
+	},
+	legend: {
+		cursor:"pointer",
+		itemclick: toggleDataSeries
+	},
+	data: [{
+		type: "column",
+		name: "Pemasukan",
+    markerSize: 5,
+    showInLegend: true,
+		visible: true,
+		yValueFormatString: "Rp#,##0.##",
+		dataPoints: <?php echo json_encode($jumlahPemasukan, JSON_NUMERIC_CHECK); ?>
+	},
+	{
+		type: "column",	
+    name: "Pengeluaran",
+    markerSize: 5,
+    showInLegend: true,
+		visible: true,
+		yValueFormatString: "Rp#,##0.##",
+		dataPoints: <?php echo json_encode($jumlahPengeluaran, JSON_NUMERIC_CHECK); ?>
+		
+	}]
+});
+chart.render();
+
+
+function toggleDataSeries(e) {
+	if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible ){
+		e.dataSeries.visible = false;
+	} else {
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
 
 }
 </script>
